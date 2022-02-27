5fe250710fe09f9ebc4bbf60a93eb50d0cf5f575
Andy <a.lee.intl@gmail.com>
2022-02-27 16:45:13 +0100

    typo

--- client/components/Main/Main.tsx
+++ client/components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Image from "next/image"
+import Image from "next/Image"
 import { RiSettings3Fill } from "react-icons/ri"
 import { AiOutlineDown } from "react-icons/ai"
 import ethLogo from "../../assets/eth.png"
