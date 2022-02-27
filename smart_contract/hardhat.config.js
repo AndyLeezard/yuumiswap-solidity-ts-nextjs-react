e0903339d12245a68840edadbf065024732034ea
Andy <a.lee.intl@gmail.com>
2022-02-27 18:28:29 +0100

    smart_contract

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/ibkgexS95zJWkYiFxIlNutkGP4xmILdr", //ETH-RINKEBY Alchemy API
+      accounts: [
+        "312b430c913add621ad38613721e98a534dfe30edec02ce7083862c8126f3ff7",
+      ], //Wallet Private Key
+    },
+  },
+}
