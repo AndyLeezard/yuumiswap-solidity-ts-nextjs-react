1226948aa991fa6124d8e2f4937931a6c0b5ce69
Andy Lee <a.lee.intl@gmail.com>
2022-03-09 00:52:37 +0100

    redacted

--- smart_contract/hardhat.config_back.js
+++ smart_contract/hardhat.config_back.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/ibkgexS95zJWkYiFxIlNutkGP4xmILdr", //ETH-RINKEBY Alchemy API
+      url: "REDACTED", //ETH-RINKEBY Alchemy API
       accounts: ["REDACTED"], // Wallet Private Key
     },
   },
