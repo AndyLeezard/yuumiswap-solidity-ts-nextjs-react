6f52de3d34156fcc31b96a403399281db937e33a
Andy Lee <a.lee.intl@gmail.com>
2022-03-08 23:59:06 +0100

    update_hardhat

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
       url: "https://eth-rinkeby.alchemyapi.io/v2/ibkgexS95zJWkYiFxIlNutkGP4xmILdr", //ETH-RINKEBY Alchemy API
       accounts: [
         "312b430c913add621ad38613721e98a534dfe30edec02ce7083862c8126f3ff7",
-      ], // Dummy Wallet Private Key for test deployment purposes - Pls don't try to hack it, it has zero balance anyway. :)
+      ], // Dummy Wallet Private Key for test deployment purposes - Pls don't try to hack it, it has zero balance anyway... :)
     },
   },
 }
